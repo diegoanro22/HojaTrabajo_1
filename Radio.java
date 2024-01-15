9215fa8665331c89cf2aec0d2eff917b79707315
diegoanro22 <diegorosalesv22@gmail.com>
2024-01-15 17:48:16 -0600

    Creacion de setters y getters y modificación de atributos

--- Radio.java
+++ Radio.java
@@ -6,27 +6,112 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Objects;
 
-//Atrivutos y listas
+//Atributos y listas
 public class Radio {
-    private static Scanner teclado = new Scanner(System.in);
-    private static ArrayList<Integer> buttonAM = new ArrayList<>(Arrays.asList(560, 610, 670, 730, 760, 790, 820, 850, 880, 910, 940, 970));
-    private static ArrayList<Double> buttonFM = new ArrayList<>(Arrays.asList(91.3, 91.7, 92.1, 92.5, 92.9, 93.3, 93.7, 93.9, 94.1, 94.5, 94.9, 95.3));
-    private static boolean state;
-    private boolean AM;
+    private boolean state;
+    private boolean aM;
     private double stationAM;
     private double stationFM;
+    private ArrayList<Double> stationsAM = new ArrayList<>();
+    private ArrayList<Double> stationsAF = new ArrayList<>();
 
 //Constructor
-public Radio(ArrayList<Integer> buttonAM, ArrayList<Double> buttonFM, boolean state, boolean AM,
-            double stationAM, double stationFM) {
-        this.buttonAM = buttonAM;
-        this.buttonFM = buttonFM;
+    public Radio(boolean state, boolean aM, double stationAM, double stationFM, ArrayList<Double> stationsAM, ArrayList<Double> stationsAF) {
         this.state = state;
-        this.AM = AM;
+        this.aM = aM;
         this.stationAM = stationAM;
         this.stationFM = stationFM;
+        this.stationsAM = stationsAM;
+        this.stationsAF = stationsAF;
     }
+
+    public Radio() {
+    }
+
+    public boolean isState() {
+        return this.state;
+    }
+
+    public boolean getState() {
+        return this.state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public boolean getAM() {
+        return this.aM;
+    }
+
+    public void setAM(boolean aM) {
+        this.aM = aM;
+    }
+
+    public double getStationAM() {
+        return this.stationAM;
+    }
+
+    public void setStationAM(double stationAM) {
+        this.stationAM = stationAM;
+    }
+
+    public double getStationFM() {
+        return this.stationFM;
+    }
+
+    public void setStationFM(double stationFM) {
+        this.stationFM = stationFM;
+    }
+
+    public ArrayList<Double> getStationsAM() {
+        return this.stationsAM;
+    }
+
+    public void setStationsAM(ArrayList<Double> stationsAM) {
+        this.stationsAM = stationsAM;
+    }
+
+    public ArrayList<Double> getStationsAF() {
+        return this.stationsAF;
+    }
+
+    public void setStationsAF(ArrayList<Double> stationsAF) {
+        this.stationsAF = stationsAF;
+    }
+
+    public Radio state(boolean state) {
+        setState(state);
+        return this;
+    }
+
+    public Radio aM(boolean aM) {
+        setAM(aM);
+        return this;
+    }
+
+    public Radio stationAM(double stationAM) {
+        setStationAM(stationAM);
+        return this;
+    }
+
+    public Radio stationFM(double stationFM) {
+        setStationFM(stationFM);
+        return this;
+    }
+
+    public Radio stationsAM(ArrayList<Double> stationsAM) {
+        setStationsAM(stationsAM);
+        return this;
+    }
+
+    public Radio stationsAF(ArrayList<Double> stationsAF) {
+        setStationsAF(stationsAF);
+        return this;
+    }
+
 
 
 /**Metodo para verificar si el radio esta encendido u apagado
