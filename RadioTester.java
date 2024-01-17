9943107c97d5504d9480e51386323e9009b3d18a
GenserCatalan <gensercatalanandre@gmail.com>
2024-01-16 20:32:12 -0600

    Se añadieron los 3 test del programa

--- RadioTester.java
+++ RadioTester.java
@@ -3,17 +3,53 @@
  *Diego Rosales - 23258
 */
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 import org.junit.Test;
 
-
 public class RadioTester {
-    
-    Radio radioTest = new Radio();
 
-    @Test 
-    public void testNextStation(){
-        assertTrue(radioTest.isAM());
+    private Radio radio;
+
+    @Before
+    public void setUp() {
+        radio = new Radio();
     }
 
+    // Prueba 1: Verificar que el método saveStation guarda correctamente una estación
+    @Test
+    public void testSaveStation() {
+        int buttonId = 1;
+        double station = 600.0;
+
+        radio.setAM(true);
+        radio.saveStation(buttonId, station);
+
+        assertEquals(station, radio.selectStation(buttonId), 0.0);
+    }
+
+    // Prueba 2: Verificar que nextStation hace que la estación vuelva al principio después de alcanzar el final    @Test
+    public void testNextStationWrapsAround() {
+        // Act
+        for (int i = 0; i < 100; i++) {
+            double previousStation = radio.nextStation();
+        }
+
+        // Assert
+        assertEquals(530, radio.getStationAM(), 0.0);
+        assertEquals(87.9, radio.getStationFM(), 0.0);
+    }
+
+    // Prueba 3: Verificar que al presionar un botón, se selecciona la estación guardada en ese botón
+    @Test
+    public void testSelectStationAfterSave() {
+        int buttonId = 3;
+        double station = 950.0;
+
+        radio.setAM(true);
+        radio.saveStation(buttonId, station);
+
+        assertEquals(station, radio.selectStation(buttonId), 0.0);
+    }
 }
